232e933d0091aa9083e275c1092c0823aac2f1be
ellag7121 <71140771+ellag7121@users.noreply.github.com>
2020-10-01 11:08:44 -0400

    Finished A3Q2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * name an the animal described after answering a series of questions
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner to get input
+    Scanner input = new Scanner(System.in);
+
+    //ask if animal has feathers
+    System.out.println("Does the animal have feathers?");
+    String featherAnswer = input.nextLine(); //store answer in a variable
+
+    if(featherAnswer.equals("yes")){ // if animal has feathers
+      //ask if animal can swim
+      System.out.println("Does the animal swim?");
+      String canSwim = input.nextLine(); //store answer in variable
+
+      if(canSwim.equals("yes")){ //if animal can swim
+        System.out.println("This is a duck"); //it is a duck
+      }else{ //if animal cannot swim
+        System.out.println("This is a hen"); //it is a hen
+      }
+    }else{ //if animal does not have feathers
+      //ask if animal has legs
+      System.out.println("Does the animal have legs?");
+      String hasLegs = input.nextLine(); //store answer in variable
+
+      if(hasLegs.equals("yes")){ //if animal has legs
+        System.out.println("This is a lizard"); //it is a lizard
+      }else{ //if animal does not have legs
+        System.out.println("This is a snake"); //it is a snake
+      }
+    }
     
   }
 }
